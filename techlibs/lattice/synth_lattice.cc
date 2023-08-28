eb892fa23c6459a8e872274b0f2b68375a019849
Miodrag Milanovic <mmicko@gmail.com>
2023-08-28 16:50:25 +0200

    Enable synth_ecp5 wrapper and copy sim files for backwards compatibility

--- techlibs/lattice/synth_lattice.cc
+++ techlibs/lattice/synth_lattice.cc
@@ -483,7 +483,6 @@ struct SynthLatticePass : public ScriptPass
 	}
 } SynthLatticePass;
 
-/*
 struct SynthEcp5Pass : public Pass
 {
 	SynthEcp5Pass() : Pass("synth_ecp5", "synthesis for ECP5 FPGAs") { }
@@ -498,6 +497,5 @@ struct SynthEcp5Pass : public Pass
 		Pass::call(design, args);
 	}
 } SynthEcp5Pass;
-*/
 
 PRIVATE_NAMESPACE_END
